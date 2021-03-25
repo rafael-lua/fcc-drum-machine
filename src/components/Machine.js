26a266b212059bae28da2f7325fee34888b69804
Rafael <rafael_oliveira@gmx.com>
2021-03-25 20:48:13 -0300

    Add sound import and onclick assignment

--- src/components/Machine.js
+++ src/components/Machine.js
@@ -2,21 +2,32 @@
 import { useState } from "react";
 import Pad from "./Pad";
 
+// Import the resource to be used
+import audio_DrumKick from "../audio/drum-kick.wav";
+import audio_DrumSnare from "../audio/drum-snare.wav"; 
+import audio_DrumClap from "../audio/drum-clap.wav";
+import audio_DrumCowbell from "../audio/drum-cowbell.wav";
+import audio_ScratchVocal from "../audio/scratch-vocal.wav";
+import audio_TrumpetLick from "../audio/trumpet-lick.wav";
+import audio_GuitarWail from "../audio/guitar-wail.wav";
+import audio_GuitarSlide from "../audio/guitar-slide.wav";
+import audio_GuitarClap from "../audio/guitar-clap.wav";
+
 const Machine = () => {
   const [display, setDisplay] = useState("Key...");
 
   return (
     <div className="machine">
       <div className="pads-grid">
-        <Pad setDisplay={setDisplay} padKey="Q" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="W" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="E" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="A" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="S" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="D" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="Z" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="X" audioFile="drum-kick.wav" padName="Drum Kick"/>
-        <Pad setDisplay={setDisplay} padKey="C" audioFile="drum-kick.wav" padName="Drum Kick"/>
+        <Pad setDisplay={setDisplay} padKey="Q" audioFile={audio_DrumKick} padName="Drum Kick"/>
+        <Pad setDisplay={setDisplay} padKey="W" audioFile={audio_DrumSnare} padName="Drum Snare"/>
+        <Pad setDisplay={setDisplay} padKey="E" audioFile={audio_DrumClap} padName="Drum Clap"/>
+        <Pad setDisplay={setDisplay} padKey="A" audioFile={audio_DrumCowbell} padName="Drum Cowbell"/>
+        <Pad setDisplay={setDisplay} padKey="S" audioFile={audio_ScratchVocal} padName="Scratch Vocal"/>
+        <Pad setDisplay={setDisplay} padKey="D" audioFile={audio_TrumpetLick} padName="Trumpet Lick"/>
+        <Pad setDisplay={setDisplay} padKey="Z" audioFile={audio_GuitarWail} padName="Guitar Wail"/>
+        <Pad setDisplay={setDisplay} padKey="X" audioFile={audio_GuitarSlide} padName="Guitar Slide"/>
+        <Pad setDisplay={setDisplay} padKey="C" audioFile={audio_GuitarClap} padName="Guitar Clap"/>
       </div>
       <hr/>
       <p className="audio-display"> {display} </p>
--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,9 +1,26 @@
 // The independent pad components
 
 const Pad = (props) => {
+
+  const handleClick = () => {
+    props.setDisplay(props.padName);
+
+    const audioContext = document.getElementById(props.padKey);
+    if (audioContext.paused) {
+      audioContext.play();
+    }else{
+      audioContext.currentTime = 0
+    }
+  }
+
   return (
-    <div className="pads-item" onClick={() => props.setDisplay(props.padName)}>
+    <div className="pads-item" onClick={handleClick}>
       <p className="pads-key">{props.padKey}</p>
+      <audio className="clip" id={props.padKey}>
+        <source src={props.audioFile} type="audio/wav"/>
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
     </div>
   )
 }
