15ea14f7f2d7c4c9149c7d8404e28771797f30ca
Rafael <rafael_oliveira@gmx.com>
2021-03-25 22:15:13 -0300

    Update pad component to listen for keypresses

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,4 +1,5 @@
 // The independent pad components
+import { useEffect } from "react";
 
 const Pad = (props) => {
 
@@ -12,6 +13,38 @@ const Pad = (props) => {
       audioContext.currentTime = 0
     }
   }
+
+  // Destruct variables from props to use it on effect hook after the component is mounted. 
+  // Then add a function callback to the whole document listening for keydown.
+  // When a key is pressed, all pad components have their handle called, but only the one with key matching will play.
+  const padKey = props.padKey;
+  const padName = props.padName;
+  const setDisplay = props.setDisplay;
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if(e.key.toLowerCase() === padKey.toLowerCase()) {
+        setDisplay(padName);
+
+        const audioContext = document.getElementById(padKey);
+        if (audioContext.paused) {
+          audioContext.play();
+        }else{
+          audioContext.currentTime = 0
+        }
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+  }, [padKey, padName, setDisplay]);
+
+  /*
+    To remember: 
+        useEffect without a second parameter run EVERY RENDER. 
+        With a empty array, run only once ever, since there is no variables being watched for update.
+        With variables references, it runs everytime that one of the variables changes.
+  */
+
 
   return (
     <div className="pads-item" onClick={handleClick}>
